88688da4a88d7489ee09df9658c65e25e2343a87
nikolasmelui <slak@samaradom.ru>
2020-08-14 21:45:45 +0400

    Add Microsoft.EntityFrameworkCore.SqlServer

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -12,8 +12,8 @@ namespace TodoApi.Controllers
   public class WeatherForecastController : ControllerBase
   {
     private static readonly string[] Summaries = new[]
-    {
-      "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+          {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
